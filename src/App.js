680ed0cf56f37ebf764e345ae4fa048c1235e3b0
ShardulVanage <vsharadul@gmail.com>
2023-04-21 06:49:36 +0530

    complete

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+
+import Example from './component/home';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Example></Example>
+    </div>
+  );
+}
+
+export default App;
